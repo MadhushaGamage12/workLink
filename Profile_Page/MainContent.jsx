ecd28630b22ae6d4cd7184afb89dab4cef3370e0
MadhushaGamage12 <madhushaudani1214@gmail.com>
2025-01-30 10:48:12 +0530

    Add files via upload

---
+++ Profile_Page/MainContent.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ProfileHeader from './ProfileHeader';
+
+
+function MainContent() {
+  return (
+    <main className="main-content">
+      <div className="profile-container">
+        <ProfileHeader />
+      </div>
+    </main>
+  );
+}
+
+export default MainContent;
